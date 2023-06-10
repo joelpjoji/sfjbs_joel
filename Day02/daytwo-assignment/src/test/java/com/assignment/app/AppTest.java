d4f5ab06596cb1f6063f3f832192125f51aa881b
Joel P Joji <joelpjoji1222@gmail.com>
2023-06-10 11:40:06 +0000

    Day2 Assignment

--- Day02/daytwo-assignment/src/test/java/com/assignment/app/AppTest.java
+++ Day02/daytwo-assignment/src/test/java/com/assignment/app/AppTest.java
@@ -1,7 +1,9 @@
 package com.assignment.app;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -9,12 +11,29 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testrev()
     {
-        assertTrue( true );
+        Reverse reverse = new Reverse();
+        String a ="Hello";
+        String results = Reverse.rev(a);
+        Assert.assertEquals(results,"olleH");
+    }
+    @Test
+    public void testpal()
+    {
+        Pallindrome pal = new Pallindrome();
+        String a ="Hello";
+        Boolean results = Pallindrome.Pal(a);
+        Assert.assertEquals(results,false);
+    }
+    @Test
+    public void testapp()
+    {
+        Append reverse = new Append();
+        String a ="Hello";
+        String b = "World";
+        String results = Append.plus(a,b);
+        Assert.assertEquals(results,"HelloWorld");
     }
 }
